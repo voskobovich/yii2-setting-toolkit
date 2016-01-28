73ee065f7a7e59362cb36d51512526ddeb15ec15
voskobovich <vitaly@voskobovich.com>
2016-01-28 19:20:19 +0200

    Dev

--- src/migrations/setting_section__code.php
+++ src/migrations/setting_section__code.php
@@ -16,9 +16,29 @@ class setting_section__code extends SettingsMigration
      */
     protected $_rows = [
         [
-            self::FIELD_SECTION => 'ga',
-            self::FIELD_KEY => 'code',
-            self::FIELD_NAME => 'Google analytics code',
+            self::FIELD_SECTION => 'code',
+            self::FIELD_KEY => 'head',
+            self::FIELD_NAME => 'Code in Head',
+            self::FIELD_VALUE => '',
+            self::FIELD_TYPE => Setting::TYPE_TEXT_AREA,
+            self::FIELD_RULES => [
+                ['string'],
+            ]
+        ],
+        [
+            self::FIELD_SECTION => 'code',
+            self::FIELD_KEY => 'body_start',
+            self::FIELD_NAME => 'Code in start Body',
+            self::FIELD_VALUE => '',
+            self::FIELD_TYPE => Setting::TYPE_TEXT_AREA,
+            self::FIELD_RULES => [
+                ['string'],
+            ]
+        ],
+        [
+            self::FIELD_SECTION => 'code',
+            self::FIELD_KEY => 'body_end',
+            self::FIELD_NAME => 'Code in end Body',
             self::FIELD_VALUE => '',
             self::FIELD_TYPE => Setting::TYPE_TEXT_AREA,
             self::FIELD_RULES => [
